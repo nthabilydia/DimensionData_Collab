06404cd1a298766d3b5eaf8f6418a6b0c74ffcca
nthabilydia <49093327+nthabilydia@users.noreply.github.com>
2020-11-19 21:17:29 +0200

    first commit

---
+++ Dimension_Data_Collab/Dimension_Data_Collab/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dimension_Data_Collab.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
